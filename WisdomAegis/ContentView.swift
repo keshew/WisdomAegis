eed9e067ecb9d37b65419cedf8a1541d85733e3f
keshew <artyom.korotckov2017@yandex.ru>
2025-10-31 16:13:06 +0500

    Initial Commit

---
+++ WisdomAegis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WisdomAegis
+//
+//  Created by Артём Коротков on 31.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WisdomAegis/WisdomAegisApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisdomAegisApp.swift
+//  WisdomAegis
+//
+//  Created by Артём Коротков on 31.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct WisdomAegisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
